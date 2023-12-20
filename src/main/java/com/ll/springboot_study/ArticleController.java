9c4bd5b22c1ac36d162597f1e5bb146c8d8c4dac
UJIN901 <dalvit9655@gmail.com>
2023-12-20 13:35:33 +0900

    article 클래스로 출력

--- src/main/java/com/ll/springboot_study/ArticleController.java
+++ src/main/java/com/ll/springboot_study/ArticleController.java
@@ -1,7 +1,10 @@
 package com.ll.springboot_study;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class ArticleController {
@@ -10,4 +13,17 @@ public class ArticleController {
     String showWrite(){
         return "article/write";
     }
+    @GetMapping("/article/doWrite")
+    @ResponseBody
+    Article doWrite(String title, String body){
+        Article article = new Article(1, title, body);
+        return article;
+    }
+}
+@AllArgsConstructor
+@Getter
+class Article{
+    private long id;
+    private String title;
+    private String body;
 }
