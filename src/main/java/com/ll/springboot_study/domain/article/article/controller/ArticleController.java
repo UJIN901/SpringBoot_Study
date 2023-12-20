55efe729728a3b2880249a0e3cccda4153314245
UJIN901 <dalvit9655@gmail.com>
2023-12-20 17:32:54 +0900

    service 생성 후 기능은 모두 service단에서 이루어진다.

--- src/main/java/com/ll/springboot_study/domain/article/article/controller/ArticleController.java
+++ src/main/java/com/ll/springboot_study/domain/article/article/controller/ArticleController.java
@@ -1,18 +1,18 @@
 package com.ll.springboot_study.domain.article.article.controller;
 
 import com.ll.springboot_study.domain.article.article.entity.Article;
+import com.ll.springboot_study.domain.article.article.service.ArticleService;
 import com.ll.springboot_study.global.rsData.RsData;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @Controller
 public class ArticleController {
-    private List<Article> articles = new ArrayList<>();
+    private final ArticleService articleService = new ArticleService();
 
     @GetMapping("/article/write")
     String showWrite(){
@@ -21,12 +21,10 @@ public class ArticleController {
 
     @PostMapping("/article/write")
     @ResponseBody
-    RsData<Article> doWrite(String title, String body){
+    RsData doWrite(String title, String body){
 
-        Article article = new Article(articles.size()+1, title, body);
+        Article article = articleService.write(title, body);
         RsData<Article> rs = new RsData<>("S-1", "%d번 게시물이 작성되었습니다.".formatted(article.getId()), article);
-
-        articles.add(article);
 
         return rs;
     }
@@ -34,13 +32,13 @@ public class ArticleController {
     @GetMapping("/article/getLastArticle")
     @ResponseBody
     Article getLastArticle(){
-        return articles.getLast();
+        return articleService.findLastArticle();
     }
 
     @GetMapping("/article/getArticles")
     @ResponseBody
     List<Article> getArticles(){
-        return articles;
+        return articleService.findAll();
     }
 }
 
---
+++ src/main/java/com/ll/springboot_study/domain/article/article/service/ArticleService.java
@@ -0,0 +1,24 @@
+package com.ll.springboot_study.domain.article.article.service;
+
+
+import com.ll.springboot_study.domain.article.article.entity.Article;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ArticleService {
+    private List<Article> articles = new ArrayList<>();
+
+    public Article write(String title, String body) {
+        Article article = new Article(articles.size()+1, title, body);
+        articles.add(article);
+    }
+
+    public Article findLastArticle() {
+        return articles.getLast();
+    }
+
+    public List<Article> findAll() {
+        return articles;
+    }
+}
