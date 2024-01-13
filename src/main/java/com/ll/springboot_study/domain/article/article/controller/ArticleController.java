d0b4f8d1a7e19f170373660579054b6c73045e48
UJIN901 <dalvit9655@gmail.com>
2024-01-13 14:32:23 +0900

    @Valid를 사용한다면, @Validated를 제거해도 된다.

--- src/main/java/com/ll/springboot_study/domain/article/article/controller/ArticleController.java
+++ src/main/java/com/ll/springboot_study/domain/article/article/controller/ArticleController.java
@@ -4,12 +4,12 @@ import com.ll.springboot_study.domain.article.article.entity.Article;
 import com.ll.springboot_study.domain.article.article.service.ArticleService;
 import com.ll.springboot_study.global.rq.Rq;
 import com.ll.springboot_study.global.rsData.RsData;
+import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.Setter;
 import org.springframework.stereotype.Controller;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -18,7 +18,6 @@ import java.util.List;
 
 @Controller
 @RequiredArgsConstructor // 사용시 final 붙은 필드에 대한 생성자를 생성해준다.
-@Validated // spring단에서 유효성 검사 해주는 어노테이션
 public class ArticleController {
     private final ArticleService articleService;
     private final Rq rq;
@@ -41,7 +40,7 @@ public class ArticleController {
 
     @PostMapping("/article/write")
     @ResponseBody
-    RsData doWrite(WriteForm writeForm){
+    RsData doWrite(@Valid WriteForm writeForm){
 
         Article article = articleService.write(writeForm.title, writeForm.body);
         RsData<Article> rs = new RsData<>("S-1", "%d번 게시물이 작성되었습니다.".formatted(article.getId()), article);
