ccaf6ff4d080ba5923ad00ff562bc60ca7973cd4
UJIN901 <dalvit9655@gmail.com>
2024-01-13 15:08:19 +0900

    test data 추가

--- src/main/java/com/ll/springboot_study/global/app/AppConfig.java
+++ src/main/java/com/ll/springboot_study/global/app/AppConfig.java
@@ -1,15 +1,7 @@
 package com.ll.springboot_study.global.app;
 
-import com.ll.springboot_study.domain.article.article.entity.Article;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
-import java.util.List;
 
 @Configuration
 public class AppConfig {
-    @Bean
-    List<Article> articles(){
-        return new java.util.ArrayList<>();
-    }
 }
